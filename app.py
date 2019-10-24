9bf9010f11ebb31827e759d94bfb6e9da9fe5b8b
Alexis-Summey <51552285+Alexis-Summey@users.noreply.github.com>
2019-10-24 18:41:47 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 import pymongo
 
@@ -29,35 +28,3 @@ def home_page():
 
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-from flask import Flask, render_template
-import pymongo
-
-app = Flask(__name__)
-
-# setup mongo connection
-# conn = "mongodb://localhost:27017"
-# client = pymongo.MongoClient(conn)
-
-# # connect to mongo db and collection
-# db = client.ufo_db
-# collection = db.ufo
-
-@app.route("/")
-def home_page():
-    # circle = mongo.db.shapes.find({"circle": True})
-    return render_template("index2.html")
-
-# @app.route("/")
-# def index():
-#     # write a statement that finds all the items in the db and sets it to a variable
-#     inventory = list(db.collection.find())
-#     print(inventory)
-
-#     # render an index.html template and pass it the data you retrieved from the database
-#     return render_template("index.html", inventory=inventory)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 65d1e4bc452ce20908cc5c6a690df3d4b39c6014
