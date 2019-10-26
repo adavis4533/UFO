1862153ccc960e5bafc949077a26e0381ac60ee8
adavis4533 <51856920+adavis4533@users.noreply.github.com>
2019-10-26 12:35:23 -0400

    darkmap

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,10 @@
 //function createMap(ufoSightings){
 
   // Create the tile layer that will be the background of our map
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.light",
+    id: "mapbox.dark",
     accessToken: API_KEY
   });
 
